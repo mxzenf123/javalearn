ff062e83cb7fab5316735f43470125f7fa2c822d
mxzenf123 <93839187@qq.com>
2022-09-17 09:54:31 +0800

    Configurable和Configuration对于事务注解配置等效

--- src/main/java/org/yangxin/transaction/Config.java
+++ src/main/java/org/yangxin/transaction/Config.java
@@ -2,6 +2,7 @@ package org.yangxin.transaction;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,7 +12,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
-@Configuration
+/**
+ * @author 杨大哥
+ * @date 2022/9/17 9:47
+ * Configuration和Configurable属性配置等效对于EnableTransactionManagement来说
+ * ，只要配置类有Import注解就会被ConfigurationClassPostProcessor解析，网上文章很多说法有误
+ */
+//@Configuration
+@Configurable
 @EnableTransactionManagement
 public class Config {
     @Bean
