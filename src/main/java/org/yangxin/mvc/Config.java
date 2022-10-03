4f5e7c98ec3a5bc9713230ee5946380ac71bb8c7
mxzenf123 <93839187@qq.com>
2022-10-03 10:32:24 +0800

    完善springboot mvc测试环境

--- src/main/java/org/yangxin/mvc/Config.java
+++ src/main/java/org/yangxin/mvc/Config.java
@@ -1,9 +1,28 @@
 package org.yangxin.mvc;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletRegistrationBean;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
 
-@Configurable
+@Configuration
 @ComponentScan
 public class Config {
+
+    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory(){
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet(){
+        return new DispatcherServlet();
+    }
+
+    @Bean
+    public DispatcherServletRegistrationBean dispatcherServletRegistrationBean(DispatcherServlet dispatcherServlet){
+        return new DispatcherServletRegistrationBean(dispatcherServlet, "/");
+    }
 }
--- src/main/java/org/yangxin/mvc/Demo1.java
+++ src/main/java/org/yangxin/mvc/Demo1.java
@@ -1,11 +1,16 @@
 package org.yangxin.mvc;
 
-import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
 
 public class Demo1 {
 
+    private static final Logger log = LoggerFactory.getLogger(Demo1.class);
+
     public static void main(String[] args) {
-        AnnotationConfigServletWebApplicationContext context =
-                new AnnotationConfigServletWebApplicationContext(Config.class);
+        AnnotationConfigServletWebServerApplicationContext context =
+                new AnnotationConfigServletWebServerApplicationContext(Config.class);
+
     }
 }
